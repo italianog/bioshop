2ba3dd97cbae126ec8388a55f43ad7d8ccb87e8b
Giovanni <58092685+italianog@users.noreply.github.com>
2020-07-05 18:32:16 +0200

    routing and withRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+
+import { Switch, Route } from "react-router-dom";
+
 import Header from "./components/header/header.component";
 import Homepage from "./pages/homepage/homepage.components";
 
+const SamplePage = () => (
+  <div>
+    <h1>Sample PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <Homepage></Homepage>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={SamplePage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -19,27 +19,27 @@ class Directory extends React.Component {
           title: "dolci",
           imageUrl: "https://i.ibb.co/Wvg9VjP/dolci.jpg",
           id: 2,
-          linkUrl: "shop/jackets",
+          linkUrl: "shop/dolci",
         },
         {
           title: "ortaggi",
           imageUrl: "https://i.ibb.co/MpsJfKb/ortaggi.jpg",
           id: 3,
-          linkUrl: "shop/sneakers",
+          linkUrl: "shop/ortaggi",
         },
         {
           title: "salse",
           imageUrl: "https://i.ibb.co/FJPT3q3/salse.jpg",
           size: "large",
           id: 4,
-          linkUrl: "shop/womens",
+          linkUrl: "shop/salse",
         },
         {
           title: "preparati",
           imageUrl: "https://i.ibb.co/Zms398Z/preparati.jpg",
           size: "large",
           id: 5,
-          linkUrl: "shop/mens",
+          linkUrl: "shop/preparati",
         },
       ],
     };
@@ -48,8 +48,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
