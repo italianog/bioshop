dc4cd308cfd13bfb3eccf0e9204bce5d5c2940a0
Giovanni <58092685+italianog@users.noreply.github.com>
2020-07-05 20:28:04 +0200

    added header component

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Header from "./components/header/header.component";
 import Homepage from "./pages/homepage/homepage.components";
 import ShopPage from "./pages/shop/shop.component";
 
-
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./header.styles.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
-const Header = () => <div className="header">
-
-    </div>;
+const Header = () => (
+  <div className="header">
+    <Link classname="logo-container" to="/">
+      <Logo className="logo"></Logo>
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
+    </div>
+  </div>
+);
 
 export default Header;
