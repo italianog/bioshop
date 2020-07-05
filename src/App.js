2763dc518b2da82259f741e957259a45f60c6fc7
Giovanni <58092685+italianog@users.noreply.github.com>
2020-07-05 15:30:56 +0200

    homepage page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/header/header.component";
+import Homepage from "./pages/homepage/homepage.components";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Homepage></Homepage>
+    </div>
+  );
 }
 
 export default App;
