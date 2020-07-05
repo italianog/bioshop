9d387ed3337af893ee8cf7b254a95bba5447ead9
Giovanni <58092685+italianog@users.noreply.github.com>
2020-07-05 19:56:22 +0200

    added collectionPreview

--- src/App.js
+++ src/App.js
@@ -6,19 +6,15 @@ import { Switch, Route } from "react-router-dom";
 
 import Header from "./components/header/header.component";
 import Homepage from "./pages/homepage/homepage.components";
+import ShopPage from "./pages/shop/shop.component";
 
-const SamplePage = () => (
-  <div>
-    <h1>Sample PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={SamplePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,249 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: "Conserve",
+    routeName: "conserve",
+    items: [
+      {
+        id: 1,
+        name: "Pomodoro",
+        imageUrl: "https://i.ibb.co/6Nydt57/OLYMPUS-DIGITAL-CAMERA.jpg",
+        price: 10,
+      },
+      {
+        id: 2,
+        name: "Melanzane",
+        imageUrl: "https://i.ibb.co/ypkgK0X/blue-beanie.png",
+        price: 18,
+      },
+      {
+        id: 3,
+        name: "Funghi",
+        imageUrl: "https://i.ibb.co/QdJwgmp/brown-cowboy.png",
+        price: 35,
+      },
+      {
+        id: 4,
+        name: "Peperoni",
+        imageUrl: "https://i.ibb.co/RjBLWxB/grey-brim.png",
+        price: 25,
+      },
+      {
+        id: 5,
+        name: "Pepi",
+        imageUrl: "https://i.ibb.co/YTjW3vF/green-beanie.png",
+        price: 18,
+      },
+      {
+        id: 6,
+        name: "Pomodori Secchi",
+        imageUrl: "https://i.ibb.co/rKBDvJX/palm-tree-cap.png",
+        price: 14,
+      },
+      {
+        id: 7,
+        name: "Red Beanie",
+        imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
+        price: 18,
+      },
+      {
+        id: 8,
+        name: "Wolf Cap",
+        imageUrl: "https://i.ibb.co/1f2nWMM/wolf-cap.png",
+        price: 14,
+      },
+      {
+        id: 9,
+        name: "Blue Snapback",
+        imageUrl: "https://i.ibb.co/X2VJP2W/blue-snapback.png",
+        price: 16,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Dolci",
+    routeName: "dolci",
+    items: [
+      {
+        id: 1,
+        name: "Cannoli",
+        imageUrl: "https://i.ibb.co/Wvg9VjP/dolci.jpg",
+        price: 220,
+      },
+      {
+        id: 2,
+        name: "Cassata",
+        imageUrl: "https://i.ibb.co/dJbG1cT/yeezy.png",
+        price: 280,
+      },
+      {
+        id: 3,
+        name: "Granita",
+        imageUrl: "https://i.ibb.co/fDqCSZn/granita.jpg",
+        price: 110,
+      },
+      {
+        id: 4,
+        name: "Cuddure",
+        imageUrl: "https://i.ibb.co/1RcFPk0/white-nike-high-tops.png",
+        price: 160,
+      },
+      {
+        id: 5,
+        name: "Nike Red High Tops",
+        imageUrl: "https://i.ibb.co/QcvzydB/nikes-red.png",
+        price: 160,
+      },
+      {
+        id: 6,
+        name: "Nike Brown High Tops",
+        imageUrl: "https://i.ibb.co/fMTV342/nike-brown.png",
+        price: 160,
+      },
+      {
+        id: 7,
+        name: "Air Jordan Limited",
+        imageUrl: "https://i.ibb.co/w4k6Ws9/nike-funky.png",
+        price: 190,
+      },
+      {
+        id: 8,
+        name: "Timberlands",
+        imageUrl: "https://i.ibb.co/Mhh6wBg/timberlands.png",
+        price: 200,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Ortaggi",
+    routeName: "ortaggi",
+    items: [
+      {
+        id: 1,
+        name: "Melanzane",
+        imageUrl: "https://i.ibb.co/XzcwL5s/black-shearling.png",
+        price: 125,
+      },
+      {
+        id: 2,
+        name: "Zucchine",
+        imageUrl: "https://i.ibb.co/mJS6vz0/blue-jean-jacket.png",
+        price: 90,
+      },
+      {
+        id: 3,
+        name: "Pomodori",
+        imageUrl: "https://i.ibb.co/N71k1ML/grey-jean-jacket.png",
+        price: 90,
+      },
+      {
+        id: 4,
+        name: "Zucchine Lunghe",
+        imageUrl: "https://i.ibb.co/s96FpdP/brown-shearling.png",
+        price: 165,
+      },
+      {
+        id: 5,
+        name: "Tan Trench",
+        imageUrl: "https://i.ibb.co/M6hHc3F/brown-trench.png",
+        price: 185,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Salse",
+    routeName: "womens",
+    items: [
+      {
+        id: 1,
+        name: "Pomodoro",
+        imageUrl: "https://i.ibb.co/7CQVJNm/blue-tank.png",
+        price: 25,
+      },
+      {
+        id: 2,
+        name: "Pomodorino",
+        imageUrl: "https://i.ibb.co/4W2DGKm/floral-blouse.png",
+        price: 20,
+      },
+      {
+        id: 3,
+        name: "Pomodori Ramati",
+        imageUrl: "https://i.ibb.co/KV18Ysr/floral-skirt.png",
+        price: 80,
+      },
+      {
+        id: 4,
+        name: "Ciliegino",
+        imageUrl: "https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png",
+        price: 80,
+      },
+      {
+        id: 5,
+        name: "Striped Sweater",
+        imageUrl: "https://i.ibb.co/KmSkMbH/striped-sweater.png",
+        price: 45,
+      },
+      {
+        id: 6,
+        name: "Yellow Track Suit",
+        imageUrl: "https://i.ibb.co/v1cvwNf/yellow-track-suit.png",
+        price: 135,
+      },
+      {
+        id: 7,
+        name: "White Blouse",
+        imageUrl: "https://i.ibb.co/qBcrsJg/white-vest.png",
+        price: 20,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Preparati",
+    routeName: "preparati",
+    items: [
+      {
+        id: 1,
+        name: "Braciole",
+        imageUrl: "https://i.ibb.co/xJS0T3Y/camo-vest.png",
+        price: 325,
+      },
+      {
+        id: 2,
+        name: "Braciole di Pesce Spada",
+        imageUrl: "https://i.ibb.co/qMQ75QZ/floral-shirt.png",
+        price: 20,
+      },
+      {
+        id: 3,
+        name: "Alici sott'olio",
+        imageUrl: "https://i.ibb.co/55z32tw/long-sleeve.png",
+        price: 25,
+      },
+      {
+        id: 4,
+        name: "Sarde a beccafico",
+        imageUrl: "https://i.ibb.co/RvwnBL8/pink-shirt.png",
+        price: 25,
+      },
+      {
+        id: 5,
+        name: "Jean Long Sleeve",
+        imageUrl: "https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png",
+        price: 40,
+      },
+      {
+        id: 6,
+        name: "Burgundy T-shirt",
+        imageUrl: "https://i.ibb.co/mh3VM1f/polka-dot-shirt.png",
+        price: 25,
+      },
+    ],
+  },
+];
+
+export default SHOP_DATA;
