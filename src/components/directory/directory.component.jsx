73572290c1842f254be575d02a18db14d33907f6
Giovanni <58092685+italianog@users.noreply.github.com>
2020-07-07 21:32:48 +0200

    reduxed every component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,59 +1,22 @@
 import React from "react";
 import "./directory.styles.scss";
-
+import { connect } from "react-redux";
 import MenuItem from "../menu-item/menu-item.component";
+import { selectDirectorySections } from "../../redux/directory/directory.selector";
+import { createStructuredSelector } from "reselect";
 
-class Directory extends React.Component {
-  constructor(props) {
-    super(props);
+const Directory = ({ sections }) => {
+  return (
+    <div className="directory-menu">
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
+};
 
-    this.state = {
-      sections: [
-        {
-          title: "conserve",
-          imageUrl: "https://i.ibb.co/s3nr1qx/conserve.png",
-          id: 1,
-          linkUrl: "shop/conserve",
-        },
-        {
-          title: "dolci",
-          imageUrl: "https://i.ibb.co/Wvg9VjP/dolci.jpg",
-          id: 2,
-          linkUrl: "shop/dolci",
-        },
-        {
-          title: "ortaggi",
-          imageUrl: "https://i.ibb.co/MpsJfKb/ortaggi.jpg",
-          id: 3,
-          linkUrl: "shop/ortaggi",
-        },
-        {
-          title: "salse",
-          imageUrl: "https://i.ibb.co/FJPT3q3/salse.jpg",
-          size: "large",
-          id: 4,
-          linkUrl: "shop/salse",
-        },
-        {
-          title: "preparati",
-          imageUrl: "https://i.ibb.co/Zms398Z/preparati.jpg",
-          size: "large",
-          id: 5,
-          linkUrl: "shop/preparati",
-        },
-      ],
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,4 +1,45 @@
-const 
+const INITIAL_STATE = {
+  sections: [
+    {
+      title: "conserve",
+      imageUrl: "https://i.ibb.co/s3nr1qx/conserve.png",
+      id: 1,
+      linkUrl: "shop/conserve",
+    },
+    {
+      title: "dolci",
+      imageUrl: "https://i.ibb.co/Wvg9VjP/dolci.jpg",
+      id: 2,
+      linkUrl: "shop/dolci",
+    },
+    {
+      title: "ortaggi",
+      imageUrl: "https://i.ibb.co/MpsJfKb/ortaggi.jpg",
+      id: 3,
+      linkUrl: "shop/ortaggi",
+    },
+    {
+      title: "salse",
+      imageUrl: "https://i.ibb.co/FJPT3q3/salse.jpg",
+      size: "large",
+      id: 4,
+      linkUrl: "shop/salse",
+    },
+    {
+      title: "preparati",
+      imageUrl: "https://i.ibb.co/Zms398Z/preparati.jpg",
+      size: "large",
+      id: 5,
+      linkUrl: "shop/preparati",
+    },
+  ],
+};
 
+export const directoryReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
 
-export const directoryRedcucer
+export default directoryReducer;
